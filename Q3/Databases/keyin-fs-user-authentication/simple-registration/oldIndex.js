13ddb5cdf4454c4faebb02f98a264c22d83c6d1e
Magret <belloabiodun17@gmail.com>
2025-11-13 10:15:49 -0330

    Added validations and made the site more robust for auth purposes

--- Q3/Databases/keyin-fs-user-authentication/simple-registration/oldIndex.js
+++ Q3/Databases/keyin-fs-user-authentication/simple-registration/oldIndex.js
@@ -2,13 +2,13 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const bcrypt = require('bcrypt');
+const { request } = require('http');
 
 const saltRounds = 10;
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })); // to parse form data
 app.use(session({
@@ -16,6 +16,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
 }));
+
 
 const users = [];
 
@@ -28,35 +29,45 @@ app.post('/register', (request, response) => {
     const { username, password } = request.body;
 
     if (!username) {
-        return response.redirect('/register?error=Username is required');
+        return response.redirect('/register?error=Username is required'); 
     }
-
     if (!password) {
-        return response.redirect('/register?error=Password is required');
+        return response.redirect('/register?error=Password is required'); 
     }
 
+    console.log(`Username: ${username}, Password: ${password}`);
     const cleanedUsername = username.trim();
+    console.log(`cleanedUsername: ${cleanedUsername}, Password: ${password}`);
     if (!cleanedUsername) {
-        return response.redirect('/register?error=Username canot be empty');
+        return response.redirect('/register?error=Username cannot be empty');
     }
 
-    const isUsernameTaken = users.some((user) => user.username === cleanedUsername);
+    const isUsernameTaken = users.some((user) => user.username4 === username);
     if (isUsernameTaken) {
-        return response.redirect('/register?error=Username already in use.');
+        return response.redirect('/register?error=Username already in user.');
     }
 
+    // Password is minimum 8 characters
     if (password.length < 8) {
-        return response.redirect('/register?error=Password must be at least 8 characters in length.');
+        return response.redirect('/register?error=Password must be at least 8 characters.');
     }
 
-    const hashedPassword = bcrypt.hashSync(password, saltRounds);
+
+    // CREATE TABLE IF NOT EXISTS users (
+    //  username TEXT UNIQUE NOT NULL,
+    //  password TEXT NOT NULL
+    // );
+
+    // INSERT INTO users (username, password) VALUESS ($1, $2) RETURNING *;
 
     users.push({
         username: cleanedUsername,
-        password: hashedPassword,
+        password: bcrypt.hashSync(password, saltRounds),
     });
+    console.log(`Password: ${bcrypt.hashSync(password, saltRounds)}`)
+    console.log(`Users: ${JSON.stringify(users)}`);
 
-    return response.redirect('/');
+    return response.redirect('/login');
 });
 
 app.get('/login', (request, response) => {
@@ -66,9 +77,11 @@ app.get('/login', (request, response) => {
 
 app.post('/login', (request, response) => {
     const { username, password } = request.body;
+
     const user = users.find((user) => user.username === username);
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-        return response.redirect('/login?error=Invalid credentials entered.');
+    // if (!user || user.password !== password) {
+    if (!user || !bcrypt.compareSync(password, user.password)) { // THIS PART
+        return response.redirect('/login?error=Invalid credentials entered. ');
     }
 
     request.session.username = username;
@@ -79,36 +92,34 @@ app.get('/', (request, response) => {
     const username = request.session.username;
     const errorMessage = request.session.errorMessage;
     request.session.errorMessage = null;
+    // response.render('index', { username, errorMessage });
 
     if (!!username) {
-        return response.redirect('/authenticatedIndex');
+        return response.render('login', {errorMessage})
     }
-
-    response.render('index', { errorMessage });
 });
 
 app.get('/authenticatedIndex', (request, response) => {
     const username = request.session.username;
 
     if (!username) {
-        request.session.errorMessage = 'You must be logged in to do that.'
+        request.session.errorMessage = "YOu must be logged in"
         return response.redirect('/');
+        // return response.redirect('/?error=YOu must be logged in');
     }
 
-    response.render('authenticatedIndex', { username });
-});
+    return response.render('authenticatedIndex', { username })
+})
 
 app.post('/logout', (request, response) => {
     request.session.destroy((error) => {
         if (!error) {
             console.error(error);
         }
-
-        return response.redirect('/');
     });
+    return response.redirect('/');
 });
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
-
