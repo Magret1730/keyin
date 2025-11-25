6682f0b5ca618e21c48ae09f9ae5d814c2ce2654
Magret <belloabiodun17@gmail.com>
2025-11-25 09:58:27 -0330

    Completed sso sign in in class

--- Q3/Fullstack/class/keyin-fs-single-sign-on/github-sso/index.js
+++ Q3/Fullstack/class/keyin-fs-single-sign-on/github-sso/index.js
@@ -6,8 +6,41 @@ const path = require("path");
 
 const app = express();
 
-// Mock user database
 const users = [];
+
+passport.use(
+    'github',
+    new GitHubStrategy(
+        {
+            clientID: "Ov23liT3MzrYwNF0aCqC",
+            clientSecret: "e650d9a03f1e6c9bdc4a4e394b9390f48660ad5f",
+            callbackURL: "http://localhost:3000/auth/github/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+            let user = users[profile.id];
+            if (!user) {
+                let email = "No public email";
+                // Check if email is public and available
+                if (!!profile.emails && !!profile.emails[0]) {
+                    email = profile.emails[0].value;
+                }
+                user = {
+                    id: profile.id,
+                    username: profile.username,
+                    email: profile.emails?.[0]?.value || "No public email",
+                    avatar: profile.photos?.[0]?.value || "No photo",
+                };
+
+                users[profile.id] = user;
+
+                // let emails = profile.emails;
+                // emails?.[0]?.value
+            }
+            // return done("There is an error", user);
+            return done(null, user);
+        }
+    )
+);
 
 // Session configuration
 app.use(
@@ -18,6 +51,12 @@ app.use(
     })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => done(null, user.id)); // When they leave the site
+passport.deserializeUser((id, done) => done(null, users[id])); // When they come back
+
 // Set view engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -25,10 +64,14 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.get("/", (request, response) => {
+    // console.log(request.failureMessage);
     response.render("index", { user: request.user });
 });
 
-app.get("/profile", ensureAuthenticated, (request, response) => {
+app.get("/profile", (request, response) => {
+    if (!request.user) {
+        return response.redirect("/");
+    }
     response.render("profile", { user: request.user });
 });
 
@@ -40,6 +83,11 @@ app.get("/logout", (request, response) => {
         response.redirect("/");
     });
 });
+
+app.get('/auth/github', passport.authenticate("github", { scope: ["user:email"] }));
+app.get('/auth/github/callback', passport.authenticate("github", { failureRedirect: "/", failureMessage: true, successRedirect: "/profile" })
+);
+
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
