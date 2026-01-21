3ca6221fa2d2157f5759644c627c57d03278c292
Magret <belloabiodun17@gmail.com>
2026-01-21 14:34:58 -0330

    Added out-flight status

--- Q4/SDAT/UnitTest/src/main/java/org/example/Status.java
+++ Q4/SDAT/UnitTest/src/main/java/org/example/Status.java
@@ -3,6 +3,7 @@ package org.example;
 public class Status {
     public static Status LANDED_AT_GATE = new Status("landed-at-gate");
     public static Status IN_FLIGHT = new Status("in-flight");
+    public static Status OUT_FLIGHT = new Status("out-flight");
 
     private String status;
 
